bd0d942485f5fd94b782d97868e438614b52ebc7
又偷懒了 <8582982+ddLazy@user.noreply.gitee.com>
2021-07-06 21:18:46 +0800

    登录

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-button @click="logoutClick">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logoutClick() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+</style>
